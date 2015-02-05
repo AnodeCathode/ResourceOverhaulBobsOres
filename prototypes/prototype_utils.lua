362263aaf1e10ffed9791280254326df31b2608b
Johan Paulsson <gonz@badcode.net>
2015-02-04 18:29:53 -0800

    Fixes crash from PeaceMod removing the auto place of biters

--- prototypes/prototype_utils.lua
+++ prototypes/prototype_utils.lua
@@ -5,7 +5,9 @@ function is_partial()
 end
 
 function add_peak(ent, peak)
-  ent.autoplace.peaks[#ent.autoplace.peaks+1] = peak
+  if (ent.autoplace ~= null) then
+    ent.autoplace.peaks[#ent.autoplace.peaks+1] = peak
+  end
 end
 
 function change_ocataves(autoplace, octaves)

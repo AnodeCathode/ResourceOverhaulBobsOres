686b3961686d5cba3a9ef850060ecc3796df26b0
Johan Paulsson <gonz@badcode.net>
2015-02-05 08:21:39 -0800

    I forgot lua

--- prototypes/prototype_utils.lua
+++ prototypes/prototype_utils.lua
@@ -5,7 +5,7 @@ function is_partial()
 end
 
 function add_peak(ent, peak)
-  if (ent.autoplace ~= null) then
+  if (ent.autoplace ~= nil) then
     ent.autoplace.peaks[#ent.autoplace.peaks+1] = peak
   end
 end

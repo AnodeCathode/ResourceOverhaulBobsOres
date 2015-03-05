26b21bd74075f0b3d7d33680f6c1421c991e1f76
iGi <i.have@no-mail.com>
2015-03-05 19:56:02 +0100

    added yuoki industries support

--- config.lua
+++ config.lua
@@ -738,6 +738,39 @@ if remote.interfaces["peacemod"] then
   }
 end  
 
+-- yuoki industries mod
+if remote.interfaces["yuoki-ind"] then
+  config["y-res1"] = {
+    type="resource-ore",
+    allotment=55,
+    spawns_per_region={min=1, max=1},
+    size={min=7, max=15},
+    richness=8000,
+
+    starting={richness=2500, size=12, probability=0},
+
+    multi_resource_chance=0.002,
+    multi_resource={
+      ["stone"] = 8,
+    }
+  }
+	
+	config["y-res2"] = {
+    type="resource-ore",
+    allotment=50,
+    spawns_per_region={min=1, max=1},
+    size={min=7, max=12},
+    richness=7000,
+
+    starting={richness=2500, size=12, probability=0},
+
+    multi_resource_chance=0.001,
+    multi_resource={
+      ["iron-ore"] = 8,
+    }
+  }
+end
+
 --[[ commented due to absence in current version of F-Mod
 if remote.interfaces["F-Mod"] then
   -- geyser left as is for now
